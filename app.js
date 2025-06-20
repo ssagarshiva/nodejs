32637137dc4f94f254e591b685285362a874ffd5
ssagarshiva <90788068+ssagarshiva@users.noreply.github.com>
2025-06-20 10:12:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Existing route
 app.get('/', (req, res) => {
     res.send('Hello, AKS!');
+});
+
+// New route to expose "Hello World"
+app.get('/hello', (req, res) => {
+    res.send('Hello World');
 });
 
 app.listen(port, () => {
